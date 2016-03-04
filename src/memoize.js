463b02366bf1ef44baef30525e4fc84e23367984
Andrey Misaylov <andrey_misaylov@epam.com>
2016-03-04 17:07:42 +0300

    memoize func ready

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,30 @@
 export { memoize };
 
 function memoize(func) {
-	return func; // TODO
+	var cache = new Map();
+
+	function decorator() {
+		var result;
+		var key = generateKey(arguments);
+
+		if (cache.has(key)) {
+			result = cache.get(key);
+		} else {
+			result = func.apply(this, arguments);
+			cache.set(key, result);
+		}
+		return result
+	}
+	return decorator;
+}
+
+function generateKey(args) {
+	var key = 0;
+	var arr = Array.prototype.slice.call(args);
+
+	arr.forEach(function (elem, index, array) {
+		key += elem + index;
+	});
+
+	return key;
 }
